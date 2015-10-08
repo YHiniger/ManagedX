64d582fe0a8e031f8d56eb631866e506d9c7debb
Yvan J.W. HINIGER <YHiniger@users.noreply.github.com>
2015-10-08 22:07:56 +0200

    Added (partial) error codes enumeration.

---
+++ code/enumerations/ErrorCode.cs
@@ -0,0 +1,42 @@
+﻿namespace ManagedX
+{
+
+	// https://msdn.microsoft.com/en-us/library/windows/desktop/ms681382(v=vs.85).aspx
+	// WinUser.h
+	
+
+	/// <summary>Enumerates Windows error codes.</summary>
+	public enum ErrorCode : int
+	{
+		
+		/// <summary>The operation was successful.</summary>
+		None = 0,
+
+		/// <summary>Access is denied.</summary>
+		AccessDenied = 5,
+
+		/// <summary>DisplayConfig: a device attached to the system is not functioning.
+		/// </summary>
+		GenFailure = 31,
+
+		/// <summary>DisplayConfig: the request is not supported.
+		/// </summary>
+		NotSupported = 50,
+
+		/// <summary>The parameter is incorrect.</summary>
+		InvalidParameter = 87,
+
+		/// <summary>The data area passed to a system call is too small.</summary>
+		InsufficientBuffer = 122,
+
+		/// <summary>XInput: the controller is not connected.
+		/// </summary>
+		NotConnected = 1167,
+
+		/// <summary>XInput: the controller state is empty ?
+		/// </summary>
+		Empty = 4306,
+
+	}
+
+}
