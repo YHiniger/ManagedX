d8bdcc33746557e1fb8c1b0b76c8fbd62af5523f
Yvan J.W. HINIGER <YHiniger@users.noreply.github.com>
2015-11-17 02:30:15 +0100

    Added IServiceProviderEx interface.

---
+++ code/Design/IServiceProviderEx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace ManagedX.Design
+{
+
+	/// <summary>Defines methods to retrieve service objects.
+	/// <para>Inherits from <see cref="IServiceProvider"/>.</para>
+	/// </summary>
+	public interface IServiceProviderEx : IServiceProvider
+	{
+
+		/// <summary>Returns a service given its type or interface.</summary>
+		/// <typeparam name="T">Type of service.</typeparam>
+		/// <returns>Returns the service associated with the specified type.</returns>
+		T GetService<T>() where T : class;
+
+	}
+
+}
