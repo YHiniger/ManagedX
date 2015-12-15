ee196e73e8b7d4a5533de412c595849bfeb94f35
Yvan J.W. HINIGER <YHiniger@users.noreply.github.com>
2015-12-15 14:07:28 +0100

    Added PlaneIndex (for use with BoundingFrustum).

---
+++ code/enumerations/PlaneIndex.cs
@@ -0,0 +1,28 @@
+﻿namespace ManagedX
+{
+
+	/// <summary>Enumerates planes for use with <see cref="BoundingFrustum"/>.</summary>
+	public enum PlaneIndex : int
+	{
+
+		/// <summary>The index of the near plane.</summary>
+		Near,
+
+		/// <summary>The index of the far plane.</summary>
+		Far,
+
+		/// <summary>The index of the left plane.</summary>
+		Left,
+
+		/// <summary>The index of the right plane.</summary>
+		Right,
+
+		/// <summary>The index of the top plane.</summary>
+		Top,
+
+		/// <summary>The index of the bottom plane.</summary>
+		Bottom
+
+	}
+
+}
