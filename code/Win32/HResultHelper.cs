4b60614a7d8d5abb4970cbd9e203f7b489ce205c
Yvan J.W. HINIGER <YHiniger@users.noreply.github.com>
2016-12-07 01:44:33 +0100

    Added HResultHelper class.

---
+++ code/Win32/HResultHelper.cs
@@ -0,0 +1,19 @@
+﻿namespace ManagedX.Win32
+{
+
+	/// <summary></summary>
+	public static class HResultHelper
+	{
+
+		/// <summary>Returns a value indicating whether an HRESULT is an error code.</summary>
+		/// <param name="hResult">An HRESULT.</param>
+		/// <returns>Returns true if the specified HRESULT is an error code, otherwise returns false.</returns>
+		public static bool IsError( int hResult )
+		{
+			const int Mask = unchecked((int)0x80000000);
+			return ( hResult & Mask ) == Mask;
+		}
+
+	}
+
+}
